e78b9ab66f93e2cfca447158d2238fe180d6ebd7
garora <garora@login2.stampede3.tacc.utexas.edu>
2026-09-18 13:25:47 -0500

    Tried chnaging the queue limit

--- config.py
+++ config.py
@@ -119,11 +119,11 @@ MONTH_NAMES = ["Jan", "Feb", "Mar", "Apr", "May", "June",
 # Resubmission
 # ---------------------------------------------------------------
 # Largest number of jobs submitted in one run of resubmit.py
-MAX_SUBMIT = 3
+MAX_SUBMIT = 1000
 
 # Largest number of my jobs allowed in the queue at once.
 # Set to 0 to turn this limit off.
-QUEUE_LIMIT = 5
+QUEUE_LIMIT = 15
 
 # ---------------------------------------------------------------
 # Relaxation restart
